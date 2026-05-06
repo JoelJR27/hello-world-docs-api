a9f93a6b16106f82a855044ee56a3c2fd95404f2
Joel Júnior <devjoeljunior@gmail.com>
2026-05-06 18:57:34 -0300

    commit inicial

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import express, { type Application } from 'express';
+import errorHandler from '../shared/middlewares/error-handler.middleware.js';
+import { LoginDTO } from '../modules/auth/dto/login.dto.js';
+
+export const app: Application = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    return response.status(200).send({ message: "API rodando com sucesso!" })
+});
+
+app.use(errorHandler);
+
+export default app;
