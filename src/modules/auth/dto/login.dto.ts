592cd2050d5cd30c84d4249f4e4ce2af1f1abb6b
Joel Júnior <devjoeljunior@gmail.com>
2026-05-13 20:09:44 -0300

    criar DTO de login

---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { AbstractDTO } from "../../../shared/utils/abstract.dto.js";
+
+const loginSchema = z.object({
+    email: z.email({ error: "Email inválido." }).trim().max(255, { error: "O email deve ter no máximo 255 caracteres." }),
+    password: z
+        .string({ error: "A senha deve ser do formato texto." })
+        .min(6, { error: "A senha deve conter no mínimo 6 caracteres." })
+        .regex(/[A-Z]/, { error: "A senha deve conter ao menos uma letra maiúscula." })
+        .regex(/[a-z]/, { error: "A senha deve conter ao menos uma letra minúscula." })
+        .regex(/[0-9]/, { error: "A senha deve conter ao menos um número." })
+        .regex(/[^A-Za-z0-9]/, { error: "A senha deve conter ao menos um caractere especial." }).regex(/^\S+$/, {
+            error: "A senha não pode conter espaços.",
+        }).max(25, { error: "A senha deve ter no máximo 25 caracteres." })
+})
+
+export class LoginDTO extends AbstractDTO<typeof loginSchema> {
+
+    rules() {
+        return loginSchema
+    }
+}
