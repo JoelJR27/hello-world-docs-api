69dd4afd238d0073224d90662cd547a346a225fa
Joel Júnior <devjoeljunior@gmail.com>
2026-05-14 22:16:06 -0300

    refactor: alterar obrigatoriedade dos campos 'content, categoryId e subcategoryId'

--- src/modules/article/dto/create-article.dto.ts
+++ src/modules/article/dto/create-article.dto.ts
@@ -4,10 +4,12 @@ import { AbstractDTO } from "../../../shared/utils/abstract.dto.js";
 export const createArticleSchema = z.object({
     title: z.string({ error: "Formato do título é inválido." }).trim().min(5, "Título deve conter pelo menos 5 caracteres").max(100, "Título deve conter no máximo 100 caracteres"),
     slug: z.string({ error: "Formato do slug é inválido." }).trim().min(5, "Slug deve conter pelo menos 5 caracteres").max(100, "Slug deve conter no máximo 100 caracteres").regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "Slug deve conter apenas letras minúsculas, números e hífens"),
-    content: z.json({ error: "Conteúdo do artigo deve ser um JSON válido." }),
+    content: z.json({ error: "Conteúdo do artigo deve ser um JSON válido." }).refine((value) => value !== null, {
+        message: "Conteúdo do artigo não pode ser null.",
+    }),
     description: z.string({ error: "Formato da descrição é inválido." }).trim().max(255, "Descrição deve conter no máximo 255 caracteres").optional(),
-    categoryId: z.uuidv4({ error: "ID de categoria inválido." }).trim(),
-    subcategoryId: z.uuidv4({ error: "ID de subcategoria inválido." }).trim(),
+    categoryId: z.uuidv4({ error: "ID de categoria inválido." }).trim().optional(),
+    subcategoryId: z.uuidv4({ error: "ID de subcategoria inválido." }).trim().optional(),
 })
 
 export type CreateArticleDTOType = z.infer<typeof createArticleSchema>;
